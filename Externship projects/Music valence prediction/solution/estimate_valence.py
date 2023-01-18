e4f0d782b7624d46c446ceb08067d222dd50ed61
Ivan Romadin <ioan.romadin@gmail.com>
2023-01-18 17:46:47 +0300

    minor updates for the valence estimation script

--- Externship projects/Music valence prediction/solution/estimate_valence.py
+++ Externship projects/Music valence prediction/solution/estimate_valence.py
@@ -37,20 +37,24 @@ def get_features(file_path, duration=25):
     cent_feature = get_stats(cent)
 
     rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr, roll_percent=0.95)
-    rolloff_middle = librosa.feature.spectral_rolloff(y=y, sr=sr, roll_percent=0.5)
-    rolloff_min = librosa.feature.spectral_rolloff(y=y, sr=sr, roll_percent=0.01)
+    rolloff_middle = librosa.feature.spectral_rolloff(
+        y=y, sr=sr, roll_percent=0.5)
+    rolloff_min = librosa.feature.spectral_rolloff(
+        y=y, sr=sr, roll_percent=0.01)
     rolloff_feature = np.concatenate(
         (get_stats(rolloff), get_stats(rolloff_middle), get_stats(rolloff_min))
     )
 
-    contrast = librosa.feature.spectral_contrast(S=np.abs(librosa.stft(y=y)), sr=sr)
+    contrast = librosa.feature.spectral_contrast(
+        S=np.abs(librosa.stft(y=y)), sr=sr)
     contrast_feature = get_stats(contrast)
 
     chroma_cens = librosa.feature.chroma_cens(y=y, sr=sr)
     chroma_cens_feature = get_stats(chroma_cens)
 
     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
-    dtempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr, aggregate=None)
+    dtempo = librosa.beat.tempo(
+        onset_envelope=onset_env, sr=sr, aggregate=None)
     dtempo_feature = get_stats(dtempo)
 
     zcr = librosa.feature.zero_crossing_rate(y)
@@ -76,6 +80,10 @@ def get_features(file_path, duration=25):
 
 
 def estimate_valence(file_path):
+    """
+    Takes in an `.mp3` or `.wav` file and returns the valence,
+    the lower and upper boundaries of the 95% confidence interval.
+    """
 
     features = get_features(file_path)
     model = joblib.load("CatBoost_daem_data.pkl")
@@ -87,5 +95,3 @@ def estimate_valence(file_path):
     upper = valence + CI
 
     return valence, lower, upper
-
-
